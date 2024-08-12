b5382a4205ae70775cd90c36ca2a8c0e48e1367a
Param-Kumar Jadvani <jadvaniparam@gmail.com>
2024-08-12 10:36:45 +0530

    databaseupdate

--- DataBase/db.js
+++ DataBase/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
 const dbConnect = async () => {
-  await mongoose.connect("mongodb+srv://Jay-Kalsariya:Jay@mvc-node-js.bpk8opa.mongodb.net/?retryWrites=true&w=majority&appName=MVC-NODE-JS");
-  console.log('DB connection established');
+  await mongoose.connect(
+    "mongodb+srv://Jay-Kalsariya:Jay@mvc-node-js.bpk8opa.mongodb.net/?retryWrites=true&w=majority&appName=MVC-NODE-JS"
+  );
+
+  console.log("DB connection established");
 };
 
 module.exports = dbConnect;
