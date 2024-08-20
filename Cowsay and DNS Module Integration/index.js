bca3eed256a98e397d7e3a6777a0eff593102b7a
Param-Kumar Jadvani <jadvaniparam@gmail.com>
2024-08-20 13:10:35 +0530

    DNS

--- Cowsay and DNS Module Integration/index.js
+++ Cowsay and DNS Module Integration/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cowsay = require("cowsay");
+const dns = require("node:dns");
 const app = express();
 const PORT = 3118;
 app.use(express.json());
@@ -14,6 +15,22 @@ app.get("/cowsay", (req, res) => {
   );
 
   res.send({ message: "Thank for using cowsay!" });
+});
+
+
+app.get("/resolve", (req, res) => {
+  const domain = req.query.domain;
+
+  if (!domain) {
+    return res.status(400).json({ error: "Domain name is required" });
+  }
+
+  dns.lookup(domain, (err, address, family) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to resolve domain" });
+    }
+    res.json({ domain, address, family });
+  });
 });
 
 app.listen(PORT, () => {
