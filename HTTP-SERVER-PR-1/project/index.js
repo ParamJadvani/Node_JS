2f8b434db3dc5df4e925c1867ab8af1fea3f0637
Param-Kumar Jadvani <jadvaniparam@gmail.com>
2024-09-01 11:17:50 +0530

    HTTP Module Test Compelete

--- HTTP-SERVER-PR-1/project/index.js
+++ HTTP-SERVER-PR-1/project/index.js
@@ -1,26 +1,20 @@
 const http = require("http");
 const fs = require("fs");
-
 const PORT = 8090;
 
 const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "Text/Html" });
   if (req.url === "/") {
     fs.readFile("index.html", (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/html" });
-        res.end("500 Internal Server Error");
-        return;
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
+      if (err) return res.end("Server Error");
+
       res.end(data);
     });
   } else if (["/login", "/signup", "/contact", "/service"].includes(req.url)) {
-    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(
       JSON.stringify({ message: `This is the ${req.url.substring(1)} route` })
     );
   } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
     res.end("404 Not Found");
   }
 });
