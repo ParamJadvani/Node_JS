db69534db9545ce96e3d93e64d1bad0da8bcb69f
Param-Kumar Jadvani <jadvaniparam@gmail.com>
2024-09-21 11:29:43 +0530

    code-update

--- CRUD in MongoDB/index.js
+++ CRUD in MongoDB/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const connect_DB = require("./config/DataBase");
 const userRouter = require("./routes/user_routes");
-const app = express();
-const path = require("path");
-const ejs = require("ejs");
-const cors = require("cors");
 const isAuth = require("./middlewares/user_middlewares");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
 const PORT = 3118;
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Parse form data
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 
@@ -26,5 +26,10 @@ app.set("views", path.join(__dirname, "view"));
 app.use("/users", userRouter);
 app.get("/", isAuth, (req, res) => res.render("index"));
 
-// Connect to database and then start the server
-app.listen(PORT, connect_DB());
+// Connect to database and start the server
+app.listen(PORT, () => {
+  connect_DB();
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
